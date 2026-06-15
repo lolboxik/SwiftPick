afc376ab02a0c6ed9c1866c6557d45c4babaec2e
lolboxik <stepan@shebanin.ru>
2026-06-15 17:30:21 +0300

    Revert "Support Railway deployment ports"
    This reverts commit 8f20b1257390f4eb78f536ddece9fb41d23152a2.

--- SwiftPick.API/Program.cs
+++ SwiftPick.API/Program.cs
@@ -14,12 +14,6 @@ using SwiftPick.Services.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var port = Environment.GetEnvironmentVariable("PORT");
-if (!string.IsNullOrWhiteSpace(port))
-{
-    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-}
-
 // Add services to the container.
 
 // AutoMapper
